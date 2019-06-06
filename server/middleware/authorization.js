60127f44f2442a03a7fb7906ae720eae7d9ef847
trantien <tientv@cbex.xyz>
2019-06-07 00:09:05 +0700

    update authen

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -5,22 +5,30 @@ let LoopBackContext = require('loopback-context');
 module.exports = function() {
   return async(req, res, next) => {
     let ctx = LoopBackContext.getCurrentContext()
-    
+    let err = new Error();
     let token = req.headers['authorization']
     if (!token) {
-      return next(new Error('Invalid token'))
+      err.message = 'Token not exists';
+      return next(err)
     }
     
     token = token.replace('Bearer ', '')
-    const data = jwt.verify(token, 'test')
-    if (!data) {
-      return next(new Error('Invalid token or expired'))
+    let data = null
+    try {
+      data = await jwt.verify(token, 'test')
+    }catch (e) {
+      err.message = 'Token not exists';
+      return next(err)
     }
-   
-    // ctx.set('user', {
-    //   id: data.data.id,
-    //   email: data.data.email
-    // })
+    
+    if (!data) {
+      err.message = 'Invalid token or expired';
+      return next(err)
+    }
+    await ctx.set('user', {
+      id: data.data.id,
+      email: data.data.email
+    })
     return next()
   }
 }
